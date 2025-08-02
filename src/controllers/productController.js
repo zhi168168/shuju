83c63b4bf57b52c47364e055d2942e58768602dc
zhi168168 <example@example.com>
2025-08-02 17:48:57 +0800

    🐛 修复Linux大小写敏感问题
    - 修复模块导入路径：../models/product → ../models/Product
    - 解决Cannot find module '../models/product'错误
    - 确保服务在Linux系统上正常运行

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Product = require('../models/product');
+const Product = require('../models/Product');
 const crawlerService = require('../services/crawler');
 const moment = require('moment');
 const { validationResult } = require('express-validator');
